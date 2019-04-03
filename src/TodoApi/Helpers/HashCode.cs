75349787b17543f6f3b8d0993fab90405522e065
Jannes Coetzee <jannes@k2.com>
2019-04-03 14:29:50 +0200

    Add AsyncHelper and HashCode.

---
+++ src/TodoApi/Helpers/HashCode.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace TodoApi.Helpers
+{
+    public struct HashCode
+    {
+        private readonly int _value;
+
+        private HashCode(int value)
+        {
+            this._value = value;
+        }
+
+        public static implicit operator int(HashCode hashCode)
+        {
+            return hashCode._value;
+        }
+
+        public static HashCode Of<T>(T item)
+        {
+            return new HashCode(GetHashCode(item));
+        }
+
+        public HashCode And<T>(T item)
+        {
+            return new HashCode(CombineHashCodes(this._value, GetHashCode(item)));
+        }
+
+        public HashCode AndEach<T>(IEnumerable<T> items)
+        {
+            if (items == null)
+            {
+                return this;
+            }
+
+            var hashCode = items.Any() ? items.Select(GetHashCode).Aggregate(CombineHashCodes) : 0;
+            return new HashCode(CombineHashCodes(this._value, hashCode));
+        }
+
+        private static int CombineHashCodes(int h1, int h2)
+        {
+            unchecked
+            {
+                // Code copied from System.Tuple so it must be the best way to combine hash codes or at least a good one.
+                return ((h1 << 5) + h1) ^ h2;
+            }
+        }
+
+        private static int GetHashCode<T>(T item)
+        {
+            return item == null ? 0 : item.GetHashCode();
+        }
+    }
+}
